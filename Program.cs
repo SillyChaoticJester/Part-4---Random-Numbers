34ae8f999b99f17ed6183fd25c2c315bf8426bb0
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-10 14:03:03 -0500

    Putting more stuff down

--- Program.cs
+++ Program.cs
@@ -6,9 +6,18 @@
         {
             Random generator = new Random();
 
-            int number = generator.Next(); //Random Number from 0-MaxInt
-            int numberTwo = generator.Next(10); //Random Number from 0-9, does NOT add the number in the brackets
-            int numberThree = generator.Next(5, 10); //Random Number from 5-9, max number doesn't get added to the list
+            int number = generator.Next(); 
+            int numberTwo = generator.Next(10);
+            int numberThree = generator.Next(5, 10);
+
+            Console.WriteLine(number);
+            Console.WriteLine(numberTwo);
+            Console.WriteLine(numberThree);
+
+            //Next() = Produces a random number from 0 to the max number put into an integer w/o negatives
+            //Next(Int32) = Produces a random number from 0 to one less of the specified integer w/o negatives
+            //Next(Int32, Int32) = Produces a random number from within that specific range w/o negatives
+            //Next(Double) = Produces a random number from > 0.0 to < 1.0
         }
     }
 }
