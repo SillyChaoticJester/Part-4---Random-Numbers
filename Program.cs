940f8806aae7c2273223af3091963ea2b7de44a5
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-10 13:46:05 -0500

    Working on the tutorial

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random generator = new Random();
+
+            int number = generator.Next(); //Random Number from 0-MaxInt
+            int numberTwo = generator.Next(10); //Random Number from 0-9, does NOT add the number in the brackets
+            int numberThree = generator.Next(5, 10); //Random Number from 5-9, max number doesn't get added to the list
         }
     }
 }
